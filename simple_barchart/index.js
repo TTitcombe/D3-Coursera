40ae97e67c69cddaf7a922b22d3b45a4fcd17d43
Tom Titcombe <t.j.titcombe@gmail.com>
2020-03-16 21:45:37 +0000

    Linearly scale bars to fit figure
    Determine scale dynamically from data values

--- simple_barchart/index.js
+++ simple_barchart/index.js
@@ -1,12 +1,31 @@
 d3.csv("earnings.csv").then(function(data) {
+  populateTable(data);
+}).catch(function(error) {
+  d3.select("#error").text(error);
+})
+
+function populateTable(data) {
+  // Convert Earnings from strings to numbers
+  data.forEach(function(d) {
+    d.Earnings = +d.Earnings;
+  });
+
+  // Get bounds of data
+  let max_earnings = d3.max(data, function(d) { return d.Earnings; });
+  let min_earnings = 0;
+
+  // Scale data linearly to fit into figure
+  var scale = d3.scaleLinear()
+  		.domain([min_earnings, max_earnings])
+		.range([0, 300])
+
   d3.select(".chart")
     .selectAll("div")
     .data(data)
       .enter()
       .append("div")
-      .style("width", function(d) { return d.Earnings * 2 + "px"; })
+      .style("width", function(d) { return scale(d.Earnings) + "px"; })
       .text(function(d) { return '$ ' + d.Earnings; });
-}).catch(function(error) {
-})
+}
 
 
