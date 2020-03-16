299776fe7398ec7aadedf60a3d16df1eccbac6d6
Tom Titcombe <t.j.titcombe@gmail.com>
2020-03-16 21:08:29 +0000

    Switch bar chart to getting data from csv file

--- simple_barchart/index.js
+++ simple_barchart/index.js
@@ -1,9 +1,12 @@
-let data = [50, 100, 123, 64];
+d3.csv("earnings.csv").then(function(data) {
+  d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+      .enter()
+      .append("div")
+      .style("width", function(d) { return d.Earnings * 2 + "px"; })
+      .text(function(d) { return '$ ' + d.Earnings; });
+}).catch(function(error) {
+})
 
-d3.select(".chart")
-  .selectAll("div")
-  .data(data)
-    .enter()
-    .append("div")
-    .style("width", function(d) { return d * 2 + "px"; })
-    .text(function(d) { return '$ ' + d; });
+
